aa947043a6f7c55141d9519a15574d3990624632
Andrew <0xupypr0@gmail.com>
2020-12-21 14:19:50 +0200

    Homework 2020.12.16 Upd

--- csv_test.php
+++ csv_test.php
@@ -1,15 +1,21 @@
 <?php
-$list = array(
-    array("Mike", "30", "1800"),
-    array("Paul", "22", "2000"),
-    array("Anna", "28", "2300")
-);
+//$list = array(
+//    array("Mike", "30", "1800"),
+//    array("Paul", "22", "2000"),
+//    array("Anna", "28", "2300")
+//);
+
+$arData = [
+    ['name' => 'Mike', 'age' => 30, 'salary' => 1800],
+    ['name' => 'Paul', 'age' => 22, 'salary' => 2000],
+    ['name' => 'Anna', 'age' => 28, 'salary' => 2300],
+];
 
 header('Content-Type: text/csv');
 header('Content-Disposition: attachment; filename=file.csv');
 $out = fopen('php://output', 'w');
 
-foreach ($list as $line) {
+foreach ($arData as $line) {
     fputcsv($out, $line);
 }
 
